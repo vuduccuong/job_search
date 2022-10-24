31294f8318640cb932bdb1ccfc3c270523d405a6
Vu Duc Cuong <vuduccuong.ck48@gmail.com>
2022-10-24 07:13:56 +0700

    Feat: Youtube with pytube

--- src/jobsearch_tts/router.py
+++ src/jobsearch_tts/router.py
@@ -5,6 +5,8 @@ from io import BytesIO
 from fastapi import APIRouter
 from gtts.tts import gTTS
 from fastapi.responses import StreamingResponse
+from pytube import Search, YouTube
+from pytube.exceptions import VideoUnavailable
 
 router = APIRouter(prefix="/tts", tags=["Text To Speed"])
 
@@ -17,3 +19,23 @@ async def text_to_speed(r):
     mp3.seek(0)
 
     return StreamingResponse(mp3, media_type="audio/mp3")
+
+
+@router.get("/youtube")
+async def search_youtube(q: str):
+    s = Search(q)
+    return dict(data=s.results)
+
+
+@router.get("youtube_to_mp3")
+async def youtube_to_mp3(url: str):
+    try:
+        yt = YouTube(url)
+        return dict(
+            data=yt.streams.filter(progressive=True, file_extension="mp4")
+            .order_by("resolution")
+            .desc()
+            .first()
+        )
+    except VideoUnavailable:
+        raise LookupError("Video is unavaialable")
